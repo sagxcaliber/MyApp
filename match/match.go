b7441340a1199459f3bdff46c7e32d540aa997df
PybySa <sagar@mysticmatrix.in>
2024-07-08 21:55:51 +0530

    added fight with decideTurn

--- match/match.go
+++ match/match.go
@@ -24,3 +24,28 @@ func RollDie() int {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(6) + 1
 }
+
+
+// Fight simulates a fight between two players and returns the result
+func Fight(playerA, playerB *Player) string {
+	for playerA.Health > 0 && playerB.Health > 0 {
+		attackFirst, defendFirst := decideTurn(playerA, playerB)
+		attack(attackFirst, defendFirst)
+		if defendFirst.Health <= 0 {
+			return fmt.Sprintf("%s wins!", attackFirst.Name)
+		}
+		attack(defendFirst, attackFirst)
+		if attackFirst.Health <= 0 {
+			return fmt.Sprintf("%s wins!", defendFirst.Name)
+		}
+	}
+	return "It's a draw!"
+}
+
+// decideTurn decides which player attacks first based on health
+func decideTurn(playerA, playerB *Player) (*Player, *Player) {
+	if playerA.Health < playerB.Health {
+		return playerA, playerB
+	}
+	return playerB, playerA
+}
