862f83cea6c69ca10f379ca3f53933fdd633fa25
PybySa <sagar@mysticmatrix.in>
2024-07-09 08:48:11 +0530

    added code in python for myDemo

---
+++ codeInPython/main.py
@@ -0,0 +1,17 @@
+def main():
+    """
+    Creating this function to execute the main file with the following code
+
+    player1 need to be created first
+    then will create player2
+    after that we will call the fight function to get the result
+    :return: we will get the result of fight
+    """
+
+    player1 = ''
+    player2 = ''
+
+    # Need to add fight logic here
+    result = ''
+
+    return result
